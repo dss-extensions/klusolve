2319c731988772e8a490e253b90e736871ee8351
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2019-03-04 01:17:41 -0300

    Prepare for the first GitHub deployment

---
+++ ci/pack_travis.sh
@@ -0,0 +1,15 @@
+if [ -n "$TRAVIS_TAG" ]; then
+    set KLUSOLVE_TAG=$TRAVIS_COMMIT
+else
+    set KLUSOLVE_TAG=$TRAVIS_TAG
+fi
+
+mkdir -p release/klusolve/lib
+cp -R lib/ release/klusolve/lib/
+cp -R include release/klusolve/
+cp LICENSE release/klusolve/
+cp README.md release/klusolve/
+cd release
+tar zcf "klusolve_${KLUSOLVE_TAG}_${KLUSOLVE_OS}_${KLUSOLVE_ARCH}.tar.gz" klusolve
+cd ..
+rm -rf release/klusolve
