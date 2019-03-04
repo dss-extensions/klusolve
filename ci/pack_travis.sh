6556429b11929414829ceab25a11e46f1a3a7343
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2019-03-04 01:44:33 -0300

    Travis-CI: Update the output filename

--- ci/pack_travis.sh
+++ ci/pack_travis.sh
@@ -1,8 +1,8 @@
-if [ -n "$TRAVIS_TAG" ]; then
-    set KLUSOLVE_TAG=$TRAVIS_COMMIT
-else
-    set KLUSOLVE_TAG=$TRAVIS_TAG
-fi
+# if [ -n "$TRAVIS_TAG" ]; then
+    # set KLUSOLVE_TAG=$TRAVIS_COMMIT
+# else
+    # set KLUSOLVE_TAG=$TRAVIS_TAG
+# fi
 
 mkdir -p release/klusolve/lib
 cp -R lib/ release/klusolve/lib/
@@ -10,6 +10,6 @@ cp -R include release/klusolve/
 cp LICENSE release/klusolve/
 cp README.md release/klusolve/
 cd release
-tar zcf "klusolve_${KLUSOLVE_TAG}_${KLUSOLVE_OS}_${KLUSOLVE_ARCH}.tar.gz" klusolve
+tar zcf "klusolve_${TRAVIS_TAG}_${KLUSOLVE_OS}_${KLUSOLVE_ARCH}.tar.gz" klusolve
 cd ..
 rm -rf release/klusolve
