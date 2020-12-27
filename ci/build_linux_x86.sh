6329f7d69048b159ba466f42afb4b67008a07e3f
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2020-12-27 19:58:51 -0300

    Upgrade Eigen
    Eigen migrated from Bitbucket to GitLab, i.e. the download links had to be updated anyway.

--- ci/build_linux_x86.sh
+++ ci/build_linux_x86.sh
@@ -3,7 +3,7 @@ set -e -x
 cd /io
 
 export SUITESPARSE_SRC=`readlink -f ./SuiteSparse`
-export EIGEN3_SRC=`readlink -f ./eigen-eigen-323c052e1731`
+export EIGEN3_SRC=`readlink -f ./eigen-${EIGEN_VERSION}`
 
 # Build KLUSolve
 cd /io
@@ -11,7 +11,7 @@ rm -rf klusolve/build
 ls -lR klusolve
 mkdir /io/klusolve/build
 ln -s SuiteSparse klusolve/build/
-ln -s eigen-eigen-323c052e1731 klusolve/build/
+ln -s eigen-${EIGEN_VERSION} klusolve/build/
 cd /io/klusolve/build
 cmake -DUSE_SYSTEM_SUITESPARSE=OFF -DUSE_SYSTEM_EIGEN3=OFF -DCMAKE_CXX_COMPILER_ARG1=-m32 -DCMAKE_C_COMPILER_ARG1=-m32 ..
 cmake --build . --config Release
